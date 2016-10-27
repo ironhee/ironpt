a10dfdb14cc45212686fe3bcc6a7a64a879d30ab
ironhee <iron@ediket.com>
2016-10-27 18:45:36 +0900

    fix test

--- bin/test.js
+++ bin/test.js
@@ -10,9 +10,4 @@ require('dotenv').config({silent: true})
 const jest = require('jest')
 const argv = process.argv.slice(2)
 
-// Watch unless on CI
-if (!process.env.CI) {
-  argv.push('--watch')
-}
-
 jest.run(argv)
